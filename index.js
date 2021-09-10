2220872b99a0296dcda5c420ad12575e52d6ddaf
Christopher <c.barnhurst@live.com>
2021-09-09 21:37:07 -0500

    commit tree 18b49a6e94d2ceffe5ce38f572633d2c27dc0350

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'bobby']
+
+function findMatching(drivers, name) {
+    return drivers.filter(function(el) {
+        return el.toLowerCase().indexOf(name.toLowerCase()) !== -1
+    })
+  } 
+
+
+function matchName(drivers, name){
+      return drivers.filter(function(el){
+        return el.name === name
+      })
+    }
+
+function fuzzyMatch (drivers, name){
+  return drivers.filter(element =>
+  element.slice(0,1) === name.slice(0,1))
+  }
+
